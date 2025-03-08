77019618b7833e8694b362e58cd5368d2e43a522
Mahmut <miftarosky@hotmail.com>
2025-03-08 17:09:48 +0100

    feat: add aud ex 2

---
+++ OS/bash/aud3/zadaca2.sh
@@ -0,0 +1,10 @@
+if [[ $1 = "1" ]]; then
+  echo "ja izbravte prvata opcija"
+elif [[ $1 = "2" ]]; then
+  echo "ja izbravte vtorata opcija"
+elif [[ $1 = '3' ]]; then
+  echo "ja izbravte tratata opcija"
+else
+  echo "ne izbravte nishto"
+  echo "obidete se povtorno !!!"
+fi
