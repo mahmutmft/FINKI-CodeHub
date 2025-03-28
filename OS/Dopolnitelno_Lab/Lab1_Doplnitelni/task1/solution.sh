3a431ac073d18d13f74ac68970ae8cbda2b62c2f
mahmutmft <miftarosky@hotmail.com>
2025-03-28 01:28:03 +0100

    feat: updated task1 Lab1

--- OS/Dopolnitelno_Lab/Lab1_Doplnitelni/task1/solution.sh
+++ OS/Dopolnitelno_Lab/Lab1_Doplnitelni/task1/solution.sh
@@ -4,14 +4,16 @@ BEGIN{
 }
 NR > 1{
     if ($3 > max) {
+    id = $1;
     max = $3;
     city = $2;
     month = $4;
     }
 }
 END {
+    print "ID", id;
     print "City:", city;
     print "Max kWh:", max;
     print "Month:", month;
 }
-' potroshuvacka.csv
+' ../files/potroshuvacka.csv
